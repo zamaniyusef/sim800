ba8a29e293e8ea450b4660a6d0a039c1e7ee1dc5
Yousef Zamani <zamaniyusef@hotmail.com>
2021-08-08 13:10:30 +0430

    Init Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sim800
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
